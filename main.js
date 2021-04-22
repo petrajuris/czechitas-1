52bedd6381f6d9679551759e81a0de8c96a8ff10
Petra Jurisova <pgardavska@seznam.cz>
2021-04-22 20:05:34 +0200

    Lekce3

---
+++ main.js
@@ -0,0 +1,24 @@
+const header = document.querySelector("header.header-news");
+
+const carouselItemCount = 4;
+
+fetch("http://localhost:3000/news.json")
+  .then((serverResponse) => serverResponse.text())
+  .then((responseText) => {
+    const data = JSON.parse(responseText);
+    populateNewsCarousel(data.articles);
+  });
+
+function populateNewsCarousel(news) {
+  for (let i = 0; i < carouselItemCount; i++) {
+    const newsValue = news[i];
+    const newsDiv = createDivForNews(newsValue);
+    header.appendChild(newsDiv);
+  }
+}
+
+function createDivForNews(newsContents) {
+  const newsArticle = document.createElement("div");
+  newsArticle.innerText = newsContents.title;
+  return newsArticle;
+}
