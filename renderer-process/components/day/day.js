3febbb61b507c7da4d71364554e81748e3391812
Petra Jurisova <pgardavska@seznam.cz>
2021-06-03 19:56:22 +0200

    Lekce 8

--- renderer-process/components/day/day.js
+++ renderer-process/components/day/day.js
@@ -1,13 +1,36 @@
 export class Day extends HTMLElement {
-  constructor(dayNumber) {
+  constructor(date) {
     super();
-    this.innerText = dayNumber;
-    this.number = dayNumber;
+    this.date = date;
+    this.number = date.getDate();
+    this.innerText = this.number;
     this.addEventListener("click", this.handleClickEvent);
   }
 
+  getDayName() {
+    switch (this.date.getDay()) {
+      case 0:
+        return "Nedele";
+      case 1:
+        return "Pondeli";
+      case 2:
+        return "Úterý";
+      case 3:
+        return "Středa";
+      case 4:
+        return "Čtvrtek";
+      case 5:
+        return "Patek";
+      case 6:
+        return "Sobota";
+
+      default:
+        break;
+    }
+  }
+
   handleClickEvent() {
-    alert("clicked day:" + this.number);
+    window.showDayModal();
   }
 }
 
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -12,7 +12,62 @@ fetch("http://localhost:3000/news.json")
 
 const mainContent = document.querySelector("section.main-content");
 
-for (let i = 1; i <= 31; i++) {
-  const day = new Day(i);
-  mainContent.appendChild(day);
+const currentDate = new Date();
+const maxDate = new Date(
+  currentDate.getFullYear(),
+  currentDate.getMonth() + 1,
+  0
+).getDate();
+
+for (let i = 1; i <= maxDate; i++) {
+  const dayDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    i
+  );
+  mainContent.appendChild(new Day(dayDate));
 }
+
+function showDayModal() {
+  const template = document.querySelector("#modal-template");
+  const modal = template.content.cloneNode(true);
+  const closeAction = () => {
+    const child = document.querySelector("section.modal-container");
+    document.body.removeChild(child);
+  };
+  modal.querySelector("#close-modal").addEventListener("click", closeAction);
+  const cancelButton = modal.querySelector("#cancel-button");
+  cancelButton.addEventListener("click", closeAction);
+  modal.querySelector("#save-button").addEventListener("click", () => {
+    const formRef = document.querySelector("#modal-form");
+    const formData = new FormData(formRef);
+    const isHoliday = formData.get("isHolidayControl") === "on";
+  });
+  document.body.appendChild(modal);
+
+  fetch("http://localhost:3000/contacts")
+    .then((serverResponse) => serverResponse.text())
+    .then((responseText) => {
+      const data = JSON.parse(responseText);
+      const contactSelect = document.querySelector("#eventAttendees");
+      data.forEach((contact) => {
+        const option = document.createElement("option");
+        option.innerText = `${contact.first_name} ${contact.last_name}`;
+        option.setAttribute("value", contact.id);
+
+        contactSelect.appendChild(option);
+      });
+    });
+
+  const genderCheckbox = document.querySelector("#limitAttendeesByGender");
+  const genderRadio = document.querySelector("#genderSelectRow");
+  genderCheckbox.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      genderRadio.classList.remove("hidden");
+    } else {
+      genderRadio.classList.add("hidden");
+    }
+  });
+}
+
+window.showDayModal = showDayModal;
